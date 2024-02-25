8d446465afa4877907d62be6c5938ff97ae0503e
Roosun <roosa.sundstrom@hotmail.fi>
2024-02-25 14:51:45 +0200

    add/delete functions

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -2,9 +2,11 @@ package com.example.bookstore.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 
 import org.springframework.ui.Model;
@@ -19,4 +21,23 @@ public class BookController {
 	    model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+	
 }
