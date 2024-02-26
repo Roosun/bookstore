c42c337dfd78a8beeada3152acfbc90ee23d696e
Roosun <roosa.sundstrom@hotmail.fi>
2024-02-26 17:55:25 +0200

    added bootstrap

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -16,28 +16,39 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 
+	// Shows all the books
 	@RequestMapping(value = {"/", "/booklist"})
 	public String bookList(Model model) {
 	    model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
 
+	// adds new book
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		return "addbook";
 	}
 
+	// save
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
 
+	// delete the book
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
         return "redirect:../booklist";
     }
+
+	// Edit book
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+    	model.addAttribute("book", repository.findById(bookId));
+    	return "editbook";
+    } 
 	
 }
